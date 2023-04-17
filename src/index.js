5d307f2544bb09c2014fe21b8849b4a2c07274d8
Saurabh Nakade <saurabh@sma.local>
2023-04-17 14:37:28 +0530

    deployment:v1

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { disableReactDevTools } from "@fvilers/disable-react-devtools";
+
+if (process.env.NODE_ENV === "production") disableReactDevTools();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
