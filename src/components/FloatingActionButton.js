89881553b2235aa79e5132a2a1afa380937d6633
Saurabh Nakade <saurabh@sma.local>
2023-04-19 10:12:44 +0530

    navlink added to floating button

--- src/components/FloatingActionButton.js
+++ src/components/FloatingActionButton.js
@@ -1,26 +1,34 @@
 import { SmallAddIcon } from "@chakra-ui/icons";
 import { IconButton, Flex, Icon } from "@chakra-ui/react";
+import { NavLink } from "react-router-dom";
 
-function FloatingActionButton() {
+function FloatingActionButton(props) {
     return (
-        <Flex position="fixed" bottom="50px" right="50px" justify="flex-end">
-            <IconButton
-                borderRadius="full"
-                fontSize="2xl"
-                aria-label="Add product"
-                icon={<Icon as={SmallAddIcon} boxSize={8} color="white" />}
-                size="lg"
-                width="60px"
-                height="60px"
-                bg="purple.500"
-                _hover={{
-                    bg: "purple.500",
-                    transform: "scale(1.2)",
-                }}
-                _focus={{ outline: "none" }}
-                transition="all 0.2s ease-out"
-            />
-        </Flex>
+        <NavLink to="/create">
+            <Flex
+                position="fixed"
+                bottom="50px"
+                right="50px"
+                justify="flex-end"
+            >
+                <IconButton
+                    borderRadius="full"
+                    fontSize="2xl"
+                    aria-label="Add product"
+                    icon={<Icon as={SmallAddIcon} boxSize={8} color="white" />}
+                    size="lg"
+                    width="60px"
+                    height="60px"
+                    bg="purple.500"
+                    _hover={{
+                        bg: "purple.500",
+                        transform: "scale(1.2)",
+                    }}
+                    _focus={{ outline: "none" }}
+                    transition="all 0.2s ease-out"
+                />
+            </Flex>
+        </NavLink>
     );
 }
 
