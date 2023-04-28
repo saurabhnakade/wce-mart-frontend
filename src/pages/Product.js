19e187167a36062442b823a74b4eba6f5698ab81
saurabhnakade <saurabh.nakade23@gmail.com>
2023-04-28 14:22:34 +0530

    single product notification+ v2:02

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -17,11 +17,13 @@ import {
     Spinner,
     Input,
     Flex,
+    IconButton,
 } from "@chakra-ui/react";
 import { FaShoppingCart } from "react-icons/fa";
 import { useParams } from "react-router-dom";
 import url from "../firebase/config";
 import AuthContext from "../context/auth-context";
+import { CheckIcon, CloseIcon } from "@chakra-ui/icons";
 
 const Product = () => {
     const id = useParams().id;
@@ -32,6 +34,8 @@ const Product = () => {
     const [isLoadingBid, setIsLoadingBid] = useState(false);
 
     const [bid, setBid] = useState("");
+
+    const [allBids, setAllBids] = useState("");
 
     const { isOpen, onOpen, onClose } = useDisclosure();
     const [product, setProduct] = useState({
@@ -48,7 +52,16 @@ const Product = () => {
                 const product = await fetch(`${url}/api/product/single/${id}`);
                 const pObj = await product.json();
 
+                const bids = await fetch(`${url}/api/bid/${id}`, {
+                    method: "GET",
+                    headers: {
+                        Authorization: "Bearer " + auth.token,
+                    },
+                });
+                const bObj = await bids.json();
+
                 setProduct(pObj);
+                setAllBids(bObj);
                 setIsLoading(false);
             } catch (err) {
                 setIsLoading(false);
@@ -63,7 +76,7 @@ const Product = () => {
             }
         };
         fetchProduct();
-    }, [id, toast]);
+    }, [id, toast, auth.token]);
 
     const handleBuy = () => {
         onOpen();
@@ -109,6 +122,7 @@ const Product = () => {
                 productsId: id,
                 biddersId: auth.id,
                 amount: bid,
+                name: auth.name,
             };
 
             await fetch(`${url}/api/bid/create`, {
@@ -130,6 +144,45 @@ const Product = () => {
             setIsLoadingBid(false);
             toast({
                 title: "Not able to place Bid",
+                status: "error",
+                duration: 3000,
+                isClosable: true,
+            });
+        }
+    };
+
+    const bidAcceptRejectHandler = async (id, accept) => {
+        try {
+            await fetch(`${url}/api/bid/delete/${id}?accept=${accept}`, {
+                method: "DELETE",
+                headers: {
+                    Authorization: "Bearer " + auth.token,
+                },
+            });
+
+            const newBids = allBids.bids.filter((b) => b.id !== id);
+            const newBObj = { bids: newBids };
+            setAllBids(newBObj);
+
+            if (accept) {
+                toast({
+                    title: "Bid Accepted",
+                    status: "success",
+                    duration: 3000,
+                    isClosable: true,
+                });
+            } else {
+                toast({
+                    title: "Bid Rejected",
+                    status: "error",
+                    duration: 3000,
+                    isClosable: true,
+                });
+            }
+        } catch (err) {
+            toast({
+                title: "Error",
+                description: "Something went wrong. Please try again later.",
                 status: "error",
                 duration: 3000,
                 isClosable: true,
@@ -230,61 +283,93 @@ const Product = () => {
                                     Negotiable:{" "}
                                     {product.negotiable ? "Yes" : "No"}
                                 </Text>
-                                {product.negotiable &&  product.sellersId !== auth.id&& (
-                                    <Box>
+                                {product.negotiable &&
+                                    product.sellersId !== auth.id && (
                                         <Box>
-                                            <Text
-                                                fontSize="lg"
-                                                fontWeight="bold"
-                                                mb={6}
-                                            >
-                                                Place a Bid
-                                            </Text>
-                                            <Flex alignItems="center">
-                                                <Input
-                                                    type="number"
-                                                    min="0"
-                                                    max="10"
-                                                    placeholder="Enter Bid Amount"
-                                                    width="200px"
-                                                    size="sm"
-                                                    mr={8}
-                                                    value={bid}
-                                                    onChange={handleBidChange}
-                                                />
-                                                <Button
-                                                    colorScheme="green"
-                                                    size="sm"
-                                                    onClick={handleBid}
+                                            <Box>
+                                                <Text
+                                                    fontSize="lg"
+                                                    fontWeight="bold"
+                                                    mb={6}
                                                 >
-                                                    {isLoadingBid ? (
-                                                        <Spinner
-                                                            size="sm"
-                                                            color="white"
-                                                        />
-                                                    ) : (
-                                                        "Submit"
-                                                    )}
-                                                </Button>
+                                                    Place a Bid
+                                                </Text>
+                                                <Flex alignItems="center">
+                                                    <Input
+                                                        type="number"
+                                                        min="0"
+                                                        max="10"
+                                                        placeholder="Enter Bid Amount"
+                                                        width="200px"
+                                                        size="sm"
+                                                        mr={8}
+                                                        value={bid}
+                                                        onChange={
+                                                            handleBidChange
+                                                        }
+                                                    />
+                                                    <Button
+                                                        colorScheme="green"
+                                                        size="sm"
+                                                        onClick={handleBid}
+                                                    >
+                                                        {isLoadingBid ? (
+                                                            <Spinner
+                                                                size="sm"
+                                                                color="white"
+                                                            />
+                                                        ) : (
+                                                            "Submit"
+                                                        )}
+                                                    </Button>
+                                                </Flex>
+                                            </Box>
+                                        </Box>
+                                    )}
+                                {product.sellersId === auth.id &&
+                                    product.bids &&
+                                    allBids.bids.map((bid) => (
+                                        <Box
+                                            key={bid.id}
+                                            pr={4}
+                                            pl={4}
+                                            pt={1}
+                                            pb={1}
+                                            mb={3}
+                                            borderRadius="md"
+                                            bg="gray.100"
+                                            display="flex"
+                                            alignItems="center"
+                                            justifyContent="space-between"
+                                        >
+                                            <Text fontSize="lg">
+                                                {bid.name} {" - "}₹ {bid.amount}
+                                            </Text>
+                                            <Flex>
+                                                <IconButton
+                                                    aria-label="Accept Bid"
+                                                    icon={<CheckIcon />}
+                                                    mr={2}
+                                                    onClick={() =>
+                                                        bidAcceptRejectHandler(
+                                                            bid.id,
+                                                            true
+                                                        )
+                                                    }
+                                                />
+                                                <IconButton
+                                                    aria-label="Reject Bid"
+                                                    icon={<CloseIcon />}
+                                                    onClick={() =>
+                                                        bidAcceptRejectHandler(
+                                                            bid.id,
+                                                            false
+                                                        )
+                                                    }
+                                                />
                                             </Flex>
                                         </Box>
-                                    </Box>
-                                )}
-                                {  product.sellersId === auth.id&&product.bids && product.bids.length > 0 && (
-                                    <Box>
-                                        <Text fontSize="lg" fontWeight="bold" mb={3}>
-                                            Bids:
-                                        </Text>
-                                        {product.bids.map((bid) => (
-                                            <Box key={bid.bidId}>
-                                                <Text fontSize="lg">
-                                                    Bidder ID: {bid.bidderId},
-                                                    Amount: ₹ {bid.amount}
-                                                </Text>
-                                            </Box>
-                                        ))}
-                                    </Box>
-                                )}
+                                    ))}
                             </Box>
                         </Box>
                     </Grid>
